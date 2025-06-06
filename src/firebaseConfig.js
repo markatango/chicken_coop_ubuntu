13be4ee78eb4a4576f71ad51b7e4fd363a6e8b40
Mark Rodighiero <markatango@gmail.com>
2025-06-05 17:08:17 -0700

    moved env vars to root file and added script to create window._env_. vars

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,16 +4,17 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore'; // Import Firestore if you plan to use it
 
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAkphFjuA9qJWV9p8wSRdbi_oTyTaVmh84",
-  authDomain: "chickencoop-6cfba.firebaseapp.com",
-  projectId: "chickencoop-6cfba",
-  storageBucket: "chickencoop-6cfba.firebasestorage.app",
-  messagingSenderId: "534064913642",
-  appId: "1:534064913642:web:b66600c7234bb7e0944b93",
-  measurementId: "G-ZRV70B09GB"
+  apiKey: window._env_.REACT_APP_APIKEY,
+  authDomain: window._env_.REACT_APP_AUTHDOMAIN,
+  projectId: window._env_.REACT_APP_PROJECTID,
+  storageBucket: window._env_.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: window._env_.REACT_APP_MESSAGINGSENDINGID,
+  appId: window._env_.REACT_APP_APPID,
+  measurementId: window._env_.REACT_APP_MEASUREMENTID
 };
 
 // Initialize Firebase
