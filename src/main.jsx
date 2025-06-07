3555235821c35f45e4ca6059056825233472aa13
Mark Rodighiero <markatango@gmail.com>
2025-06-07 08:29:03 -0700

    deleted all the google ai generated files

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,13 @@
-// non firebase code
-//------------------------
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from "./pages/App";
-// // import RootApp from './App'; // Import the RootApp component
 
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>
-// )
-
-// Firebase flow//-------------------------\
-
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import RootApp from './App'; // Import the RootApp component
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from "./pages/App";
+// import RootApp from './App'; // Import the RootApp component
 
-const container = document.getElementById('root');
-const root = ReactDOM.createRoot(container); // Create a root
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
 
-root.render(
-  <React.StrictMode>
-    <RootApp /> {/* Render the RootApp wrapped in AuthProvider */}
-  </React.StrictMode>
-);
